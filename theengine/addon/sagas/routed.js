ad870151d9f7e158171f18f65badaabf73e35e35
toran billups <toranb@gmail.com>
2017-08-24 16:38:40 -0500

    [EXAMPLE]: now using ember-dependency-lookup

--- theengine/addon/sagas/routed.js
+++ theengine/addon/sagas/routed.js
@@ -1,11 +1,13 @@
 import { delay } from 'redux-saga';
 import { call, put, takeEvery } from 'redux-saga/effects';
+import { lookup } from 'ember-dependency-lookup';
 
 function* transitionAsync() {
   yield put({type: 'TRANSITION_START'});
   yield call(delay, 2000);
   yield put({type: 'TRANSITION_END'});
-  // Ember.getOwner(this).lookup('router:main').transitionTo('dashboard');
+  yield call(delay, 2000);
+  lookup('service:-routing').transitionTo('index');
 }
 
 export function* transition() {
